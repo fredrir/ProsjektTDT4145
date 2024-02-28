36454f1c39845c94e4ab68b51f62778efcc34854
fredrir <fhansteen@gmail.com>
2024-02-28 14:18:16 +0100

    set up the db

---
+++ wackyDB.sql
@@ -0,0 +1,7 @@
+CREATE TABLE
+    costumer_profile (
+        costumer_id INT PRIMARY KEY,
+        phone_number int NOT NULL,
+        costumer_name string NOT NULL,
+        address string NOT NULL,
+    )
