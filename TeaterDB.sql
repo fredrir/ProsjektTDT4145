e2ad118c3858c598d5c24445fd8041901347cba2
NicoSjokk <114810966+NicoSjokk@users.noreply.github.com>
2024-03-12 15:39:51 +0100

    Skjelett for databaseskjema + database inserts + setescannere for gamle scene og hovedscenen

--- TeaterDB.sql
+++ TeaterDB.sql
@@ -128,32 +128,3 @@ CREATE TABLE
         FOREIGN KEY (AktNummer, StykkeID) REFERENCES Akt (Nummer, StykkeID)
     );
 
-INSERT Into
-    Sal (Salnummer, Navn, Kapasitet)
-VALUES
-    (1, 'Hovedscenen', 516);
-
-INSERT Into
-    Sal (Salnummer, Navn, Kapasitet)
-VALUES
-    (2, 'Gamle Scene', 332);
-
-INSERT Into
-    Sal (Salnummer, Navn, Kapasitet)
-VALUES
-    (3, 'Studioscenen', 150);
-
-INSERT Into
-    Sal (Salnummer, Navn, Kapasitet)
-VALUES
-    (4, 'Teaterkjelleren', 60);
-
-INSERT Into
-    Sal (Salnummer, Navn, Kapasitet)
-VALUES
-    (5, 'Teaterkafeen', 100);
-
-INSERT INTO
-    Stol (StolID, Stolnummer, Salnummer, Rad, Område)
-VALUES
-    (1, 1, 1, 1, 'Nedre');
---
+++ insert-into-db.sql
@@ -0,0 +1,29 @@
+INSERT Into
+    Sal (Salnummer, Navn, Kapasitet)
+VALUES
+    (1, 'Hovedscenen', 516);
+
+INSERT Into
+    Sal (Salnummer, Navn, Kapasitet)
+VALUES
+    (2, 'Gamle Scene', 332);
+
+INSERT Into
+    Sal (Salnummer, Navn, Kapasitet)
+VALUES
+    (3, 'Studioscenen', 150);
+
+INSERT Into
+    Sal (Salnummer, Navn, Kapasitet)
+VALUES
+    (4, 'Teaterkjelleren', 60);
+
+INSERT Into
+    Sal (Salnummer, Navn, Kapasitet)
+VALUES
+    (5, 'Teaterkafeen', 100);
+
+INSERT INTO
+    Stol (StolID, Stolnummer, Salnummer, Rad, Område)
+VALUES
+    (1, 1, 1, 1, 'Nedre');
