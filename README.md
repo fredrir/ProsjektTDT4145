6ac5a87d46344347a6e60bdebffb27a5434c8422
NicoSjokk <114810966+NicoSjokk@users.noreply.github.com>
2024-03-12 17:11:28 +0100

    Litt README og starta på inserting av oppgitt data

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # WackyDB
+
+Inkluderer: 
+- 2 saler med stoler teaterstykker, forestillinger, akter, roller, skuespillere og andre medvirkende
+- 2 forestilliger med pre-kjøpte billetter {gamle-scene.txt} og {hovedscenen.txt}
+    - Kjøpt av en "standard bruker"
+
+Muligheter:
+- Kjøpe 9 voksenbiletter til forestillingen for "Størst av alt er kjærligheten" 3. feburar
+  - Setene er på samme rad, men trenger ikke være ved siden av hverandre
+  - Får igjen:
+    - Summen av kostnaden for billettene 
+    - ikke spesifisert i oppgaven men regner med at stolen også må returneres og endre på ledige billetter
+- Ta inn en dato i python og skriver ut hvilke forestillinger som finnes på denne datoen og lister opp hvor mange biletter som er solgt (også med forestillinger uten solgte biletter)
+- SQL query som finner navn på skuespillere som opptrer i de forkjellige teaterstykkene
+  - skriver ut navn på stykke, navn på skuespiller og rolle
+- SQL query som finner hvilke forestillinger som har solgt best
+  - Skriver ut navn på forstilling, dato og antall solgte plasser sortert på antall plasser i synkende rekkefølge 
+- Python program (og SQL) som tar et skuespillernavn og finner hvilke skuespillere de har spilt med i samme akt
+  - Skriver ut nacn på begge og hvilket skuespill det var
--- insert-into-db.sql
+++ insert-into-db.sql
@@ -1,29 +1,30 @@
-INSERT Into
-    Sal (Salnummer, Navn, Kapasitet)
-VALUES
-    (1, 'Hovedscenen', 516);
+BEGIN TRANSACTION;
 
-INSERT Into
-    Sal (Salnummer, Navn, Kapasitet)
-VALUES
-    (2, 'Gamle Scene', 332);
+INSERT INTO Sal (Salnummer, Navn, Kapasitet) VALUES (1, 'Hovedscenen', 516);
+INSERT INTO Sal (Salnummer, Navn, Kapasitet) VALUES (2, 'Gamle Scene', 332);
+INSERT INTO Sal (Salnummer, Navn, Kapasitet) VALUES (3, 'Studioscenen', 150);
+INSERT INTO Sal (Salnummer, Navn, Kapasitet) VALUES (4, 'Teaterkjelleren', 60);
+INSERT INTO Sal (Salnummer, Navn, Kapasitet) VALUES (5, 'Teaterkafeen', 100);
 
-INSERT Into
-    Sal (Salnummer, Navn, Kapasitet)
-VALUES
-    (3, 'Studioscenen', 150);
+INSERT INTO Stol (StolID, Stolnummer, Salnummer, Rad, Område) VALUES (1, 1, 1, 1, 'Nedre');
 
-INSERT Into
-    Sal (Salnummer, Navn, Kapasitet)
-VALUES
-    (4, 'Teaterkjelleren', 60);
+INSERT INTO Teaterstykke (StykkeID, Navn, Salnummer) VALUES (1, 'Kongsemnene', 1);
+INSERT INTO Teaterstykke (StykkeID, Navn, Salnummer) VALUES (2, 'Størst av alt er kjærligheten', 2);
 
-INSERT Into
-    Sal (Salnummer, Navn, Kapasitet)
-VALUES
-    (5, 'Teaterkafeen', 100);
+INSERT INTO Forestilling (Tidspunkt, StykkeID) VALUES ('2024-01-02 19:00:00', 1); 
+INSERT INTO Forestilling (Tidspunkt, StykkeID) VALUES ('2024-02-02 19:00:00', 1); 
+INSERT INTO Forestilling (Tidspunkt, StykkeID) VALUES ('2024-03-02 19:00:00', 1); 
+INSERT INTO Forestilling (Tidspunkt, StykkeID) VALUES ('2024-05-02 19:00:00', 1); 
+INSERT INTO Forestilling (Tidspunkt, StykkeID) VALUES ('2024-06-02 19:00:00', 1); 
 
-INSERT INTO
-    Stol (StolID, Stolnummer, Salnummer, Rad, Område)
-VALUES
-    (1, 1, 1, 1, 'Nedre');
+INSERT INTO Forestilling (Tidspunkt, StykkeID) VALUES ('2024-03-02 18:30:00', 2); 
+INSERT INTO Forestilling (Tidspunkt, StykkeID) VALUES ('2024-06-02 18:30:00', 2); 
+INSERT INTO Forestilling (Tidspunkt, StykkeID) VALUES ('2024-07-02 18:30:00', 2); 
+INSERT INTO Forestilling (Tidspunkt, StykkeID) VALUES ('2024-12-02 18:30:00', 2); 
+INSERT INTO Forestilling (Tidspunkt, StykkeID) VALUES ('2024-13-02 18:30:00', 2); 
+INSERT INTO Forestilling (Tidspunkt, StykkeID) VALUES ('2024-14-02 18:30:00', 2);
+
+INSERT INTO PrisGruppe (
+
+COMMIT;
+
