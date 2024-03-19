7ba642ebc8cb6f11f6f4aca56ebd1a6ae1123363
sebaskem <sebaskem@stud.ntnu.no>
2024-03-19 15:05:23 +0100

    lalala

--- TeaterDB.py
+++ TeaterDB.py
@@ -22,7 +22,7 @@ def buy_9_tickets():
             SELECT B.BillettID
             FROM Billett B
             JOIN Stol S ON B.StolID = S.StolID
-            WHERE B.StykkeID = 1 AND B.BillettID NOT IN (SELECT BillettID FROM BillettKjop) AND S.Rad = ?
+            WHERE B.StykkeID = 2 AND B.BillettID NOT IN (SELECT BillettID FROM BillettKjop) AND S.Rad = ?
             LIMIT 9
         """, (rows[0][0],))
         missing_tickets = cur.fetchall()
